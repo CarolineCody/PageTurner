7f4df6c16c6a99ee49a70e7e434ad2e36bf74b62
JonathanElm <changchris3141@gmail.com>
2020-06-19 06:56:13 -0700

    only reformated content so I can paint my minis today. The 40k saga continues.

--- menu/playMenu.hpp
+++ menu/playMenu.hpp
@@ -12,13 +12,13 @@ class playMenu{
         std::vector<scene *> stories;               //Stores a reference to all the starting scene of every story.
         
     public:
-    playMenu();                                     //Default constructor that also handles and assigns what node to travel back to.
-    void setParent(titleMenu * titleM);             //Sets the parent of this menu.
-    std::vector<scene *> printMenu();               //Handles and manages all inputs and outputs of the class that is being taken in.
-    void backPedal();                               //Access the menu that came before this menu and sets that menu to the active menu.
-    void quit();                                    //Closes the program and saves users progress.
-    bool setActive;                                 //Declares this menu as the active area that the user is interacting with.
-    void setScenes(std::vector<scene *> scenes);    //Changes the scenes within the program declaration and call.
-    std::vector<tag>* ownedTags;                    //Stored reference to the users tags.
+        playMenu();                                     //Default constructor that also handles and assigns what node to travel back to.
+        void setParent(titleMenu * titleM);             //Sets the parent of this menu.
+        std::vector<scene *> printMenu();               //Handles and manages all inputs and outputs of the class that is being taken in.
+        void backPedal();                               //Access the menu that came before this menu and sets that menu to the active menu.
+        void quit();                                    //Closes the program and saves users progress.
+        bool setActive;                                 //Declares this menu as the active area that the user is interacting with.
+        void setScenes(std::vector<scene *> scenes);    //Changes the scenes within the program declaration and call.
+        std::vector<tag>* ownedTags;                    //Stored reference to the users tags.
 };
 #endif
