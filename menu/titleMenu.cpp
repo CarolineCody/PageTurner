aae8183b10201570a0eef687f620392a1fa83582
JonathanElm <changchris3141@gmail.com>
2020-06-29 06:36:40 -0700

    titleMenu commented

--- menu/titleMenu.cpp
+++ menu/titleMenu.cpp
@@ -14,24 +14,28 @@ void titleMenu::printMenu(){
     std::cout << "========================================================" << std::endl;
     std::cout << "|                 Welcome to PageTurner                |" << std::endl;
     std::cout << "========================================================" << std::endl;
-    std::cout << "| 1) Create a Story                                    |" << std::endl;
+    std::cout << "| 1) Create or Edit a Story                            |" << std::endl;
     std::cout << "| 2) Play a Story                                      |" << std::endl;
     std::cout << "| 3) Quit                                              |" << std::endl;
     std::cout << "========================================================" << std::endl;
     std::cin >> userInput;
     //Prints the menu options.
     switch(userInput){
+        //Handles editing or making a story.
         case 1:
             setActive = false;
             story->setActive = true;
             break;
+        //Handles playing a story.
         case 2:
             setActive = false;
             play->setActive = true;
             break;
+        //Quits program.
         case 3:
             quit();
             break;
+        //Otherwise input is invalidated and the program loops.
         default:{
             std::cin.ignore();
             std::cin.clear();
@@ -43,21 +47,25 @@ void titleMenu::printMenu(){
 }
 
 void titleMenu::quit(){
-        bool notDone = true;
+    bool notDone = true;
+    //Loops until the user enters 'y' or 'n' to end the loop.
     while(notDone){
         std::cout << "Are you sure you want to close this whole program? (y/n)" << std::endl;
         char input;
         std::cin >> input;
         switch(input){
+            //Upon selecting y the program ends.
             case 'y':{
                 notDone = false;
                 setActive = false;
                 break;
             }
+            //Upon selectin n the program resumes.
             case 'n':{
                 notDone = false;
                 break;
             }
+            //Clears content and stops.
             default:{
                 std::cin.clear();
                 std::cin.ignore();
